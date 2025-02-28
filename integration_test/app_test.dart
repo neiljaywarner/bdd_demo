a73a72ddc9f9aa3bacde1b5fdeec7ab07b60685f
Neil Warner <neil.warner.contractor@youversion.com>
2025-02-28 11:57:01 -0600

    chore: remove whitespace and dart format for more of a LOC comparison

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -8,26 +8,14 @@ void main() {
 
   group('end-to-end test', () {
     testWidgets('tap on the floating action button, verify counter',
-            (tester) async {
-          // Load app widget.
-         // await tester.pumpWidget(const MyApp());
-              app.main();
-
-              await tester.pumpAndSettle(const Duration(seconds: 3));
-          // Verify the counter starts at 0.
-          expect(find.text('0'), findsOneWidget);
-
-          // Finds the floating action button to tap on.
-          final fab = find.byIcon(Icons.add);
-
-          // Emulate a tap on the floating action button.
-          await tester.tap(fab);
-
-          // Trigger a frame.
-          await tester.pumpAndSettle();
-
-          // Verify the counter increments by 1.
-          expect(find.text('1'), findsOneWidget);
-        });
+        (tester) async {
+      app.main();
+      await tester.pumpAndSettle(const Duration(seconds: 3));
+      expect(find.text('0'), findsOneWidget);
+      final fab = find.byIcon(Icons.add);
+      await tester.tap(fab);
+      await tester.pumpAndSettle();
+      expect(find.text('1'), findsOneWidget);
+    });
   });
 }
