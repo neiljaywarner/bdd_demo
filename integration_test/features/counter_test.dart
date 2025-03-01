d971df994ff3158d9b0af7a99b3dfac225799833
Neil Warner <neil.warner.contractor@youversion.com>
2025-02-28 20:47:07 -0600

    2 - notes towards patrol

--- integration_test/features/counter_test.dart
+++ integration_test/features/counter_test.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import '../../test/njw_bdd_steps/the_app_is_running.dart';
+import './../../test/njw_bdd_steps/the_app_is_running.dart';
 import 'package:bdd_widget_test/step/i_see_text.dart';
 import 'package:bdd_widget_test/step/i_tap_icon.dart';
 
--- test/features/counter_test.dart
+++ test/features/counter_test.dart
@@ -4,7 +4,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../njw_bdd_steps/the_app_is_running.dart';
+import './../njw_bdd_steps/the_app_is_running.dart';
 import 'package:bdd_widget_test/step/i_see_text.dart';
 import 'package:bdd_widget_test/step/i_tap_icon.dart';
 
