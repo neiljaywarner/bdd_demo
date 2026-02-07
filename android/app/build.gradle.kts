f499806118b578d977879ac261e909326e895086
neil warner <njwandroid@gmail.com>
2026-02-07 16:58:23 -0600

    runner added,resolution force with firebenders help, not sure if that part needed or not, ./gradlew app:connectedAndroidTest -Ptarget=/Users/neil/AndroidStudioProjects/bdd_demo/android/../integration_test/features/counter_test.dart passed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -38,12 +39,22 @@ android {
     }
 }
 
+configurations.all {
+    resolutionStrategy {
+        force("androidx.test:runner:1.6.2")
+        force("androidx.test:rules:1.6.1")
+        force("androidx.test.espresso:espresso-core:3.6.1")
+    }
+}
+
 dependencies {
 
     testImplementation("junit:junit:4.13.2")
 
-    androidTestImplementation("androidx.test:runner:1.2.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test:rules:1.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
 }
 
